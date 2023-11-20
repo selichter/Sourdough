092c2ca887c0918cc2503fc0b1f9fe3e377e3d73
Sarah Lichter <lichter.sarahe@gmail.com>
2023-11-20 14:29:26 -0600

    initial project commit

---
+++ Sourdough/SourdoughApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourdoughApp.swift
+//  Sourdough
+//
+//  Created by Sarah Lichter on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SourdoughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
